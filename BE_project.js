24bf74ec6816fc29e5ed24a66ff1a7a5fb317582
Jiratchaya Fuengthanakul <89129116+JiratchayaF@users.noreply.github.com>
2023-03-01 22:47:40 +0700

    updated

--- BE_project.js
+++ BE_project.js
@@ -2,14 +2,37 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
-
-const dbConnect = mongodb.MongoClient.connect('mongodb://localhost:27017');
+const mongoose = require('mongoose');
+// connect to DB
+// const dbConnect = mongodb.MongoClient.connect('mongodb://localhost:27017/ChatBotInvoiceTest');
 
 const app = express();
 // รอcomment
 const port = process.env.PORT || 8080;
 
-app.use(express.urlencoded());
+app.use(express.json())
+// app.use(express.urlencoded());
+// app.use(bodyParser.urlencoded({extended: false}));
+// db connection
+mongoose.connect('mongodb://localhost:27017/mydb',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}, (err) => {
+    if(!err)
+    {
+        console.log("connected to db");
+    } else {
+        console.log("error");
+    }
+})
+
+app.post('/post-custmer-data', function(req,res){
+    dbConnect.then(function(db) {
+        delete req.body._id; // for safety reasons
+        db.collection('ChatBotINVTest').insertOne(req.body);
+    });    
+    res.send('Data received:\n' + JSON.stringify(req.body));
+}); 
 
 app.get('/', function(req, res, next) {
 
