d8feca4d7d600d57f2e0a39a955d2a5eb5e5abcb
Jiratchaya Fuengthanakul <89129116+JiratchayaF@users.noreply.github.com>
2023-03-20 13:51:18 +0700

    updated

--- BE_project.js
+++ BE_project.js
@@ -51,7 +51,7 @@ app.post('/update-new', function(req,res){
       taxID: req.body.ID_Number
     });
     NewCustomerData.save();
-    res.redirect(302, '/update-new'); // redirect to main page
+    res.redirect('https://liff.line.me/1657898846-EryRWbgy'); // redirect to main page
 }); 
 
 
