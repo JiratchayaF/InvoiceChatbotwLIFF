583404cd3be6e458d5e7e630bb1664837687fbf9
Jiratchaya Fuengthanakul <89129116+JiratchayaF@users.noreply.github.com>
2023-03-20 16:21:38 +0700

    updated

--- BE_project.js
+++ BE_project.js
@@ -43,7 +43,7 @@ app.get('/', function(req, res) {
 
 });
 
-app.post('/InvoiceChatbotwLIFF/LIFF/', function(req,res,next){
+app.post('/InvoiceChatbotwLIFF/LIFF', function(req,res,next){
     let NewCustomerData = new CustomerData({
       firstname: req.body.first_name,
       lastname: req.body.last_name,
