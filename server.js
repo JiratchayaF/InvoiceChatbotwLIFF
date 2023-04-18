788159a057f6fb4f43160dd20067de8988c956b5
Atita1097 <60214035+Atita1097@users.noreply.github.com>
2023-04-19 01:08:55 +0700

    redesign HTML invoice template

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { route } = require('express/lib/application');
+const MongoClient = require('mongodb').MongoClient
 // const { redirect } = require('express/lib/response');
 
+// const orderData = require('./model/database.js')
+
 // connect to DB
-const url = 'mongodb+srv://fuengjiratchaya:mongotest123@testmongo.wxnjfzh.mongodb.net/InvoiceData'
+// const url = 'mongodb+srv://fuengjiratchaya:mongotest123@testmongo.wxnjfzh.mongodb.net/invoiceData'
+const url = 'mongodb+srv://fuengjiratchaya:mongotest123@testmongo.wxnjfzh.mongodb.net/orderTransaction'
 const app = express();
 
 // Define server port
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log( `Server running on port ${port}`))
+app.listen(port, () => 
+  console.log( `Server running on port ${port}`)
+)
 
 // view engine setup
 app.set('view', express.static(path.join(__dirname, '/view')))
@@ -28,16 +34,32 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
 
+// // Retrieving data from MongoDB
+// const Schema  = mongoose.Schema
 
-// create data schema in JSON form
-const CustomerDataSchema = {
-  firstname: String,
-  lastname: String,
-  address: String,
-  taxID: String
-}
+
+// const orderData = mongoose.model('orderSchema', orderSchema);
+
+// // // Posting data from invoice request form to MongoDB
+// // crceate data schema in JSON form
+// const CustomerDataSchema = {
+//   firstname: String,
+//   lastname: String,
+//   address: String,
+//   taxID: String
+// }
 // Create schema model to input
-const CustomerData = mongoose.model("CustomerData", CustomerDataSchema)
+// const CustomerData = mongoose.model("CustomerData", CustomerDataSchema)
+
+const orderSchema = new mongoose.Schema({
+  _id: Number,
+  customerName: String,
+  date: String,
+})
+
+const orderData = mongoose.model('orderTransaction', orderSchema)
+module.exports = orderData
+
 
 mongoose.set("strictQuery", false);
 // db connection
@@ -45,13 +67,24 @@ mongoose.connect(url, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
   })
-    .then(client => {
+    .then(result => {
       console.log("Connected to MongoDB");
+      // console.log(result)
       // Perform database operations here
+      
     })
     .catch(err => {
       console.log("Failed to connect to MongoDB:", err);
     });
+
+
+orderData.find()
+    .then(data => {
+      console.log('Here is the result: ', data)
+      
+    }).catch(err => {
+      console.log(err)
+    })
 
 app.get('/', function(req, res) {
 
@@ -59,20 +92,35 @@ app.get('/', function(req, res) {
 
 });
 
-app.post('/', function(req,res,next){
-    let NewCustomerData = new CustomerData({
-      firstname: req.body.first_name,
-      lastname: req.body.last_name,
-      address: req.body.Address,
-      taxID: req.body.ID_Number
-    });
-    console.log('updated');
-    NewCustomerData.save();
-    res.redirect(302, "/"); // redirect to main page
-    console.log('POST request');
-  }); 
+// app.post('/', function(req,res,next){
+//     let NewCustomerData = new CustomerData({
+//       firstname: req.body.first_name,
+//       lastname: req.body.last_name,
+//       address: req.body.Address,
+//       taxID: req.body.ID_Number
+//     });
+//     console.log('updated');
+//     NewCustomerData.save();
+//     res.redirect(302, "/"); // redirect to main page
+//     console.log('POST request');
+//   }); 
+
+
+// // Rethrieve transaction data from MongoDB
+//  app.get('/orders', (req, res) => {
+//   console.log('routing to /orders')
+//   orderData.find()
+//   .then((client) => {
+//     res.send(client);
+//     console.log('data found!')
+//   }).catch((err) => {
+//     console.log(err)
+//   }) 
+//  })
 
 
 // run app on local server
 // app.listen(port,'0.0.0.0');
 // console.log('Server started at http://localhost:' + port);
+
+
--- view/invoiceGen.js
+++ view/invoiceGen.js
@@ -5,64 +5,66 @@ const PDFDocument = require('pdfkit');
 const ejs = require('ejs');
 
 const options = { 
-    format: 'A5',
+    format: 'A4',
     orientation: 'portrait',
-    border: '8.mm'
 };
 
-const generatePdf = async (req, res, next) => {
-    const data = JSON.parse(fs.readFileSync('mockData/data.json', 'utf8'));
-    const htmlTemplate = fs.readFileSync('taxinvoice_template.html', 'utf8');
-    const fileName = 'Invoice' + data.invoiceNumber;
-    let array =[];
+// const generatePdf = async (req, res, next) => {
+//     const data = JSON.parse(fs.readFileSync('mockData/data.json', 'utf8'));
+//     console.log('user data parse')
 
-    // Calculate total before tax, tax and subtotal after tax
-    let total = 0;
-    array.forEach(i => {
-        total += i.total
-    });
+//     const htmlTemplate = fs.readFileSync('taxinvoice_template.html', 'utf8');
+//     const fileName = 'Invoice' + data.invoiceNumber;
+//     let array =[];
 
-    const tax = (total * 7)/100;
-    const grandTotal = total + tax;
+//     // Calculate total before tax, tax and subtotal after tax
+//     let total = 0;
+//     array.forEach(i => {
+//         total += i.total
+//     });
 
-    const invoiceTemplate = Handlebars.compile(htmlTemplate);
-    const invoice_pdf = invoiceTemplate(data);
+//     const tax = (total * 7)/100;
+//     const grandTotal = total + tax;
 
-    pdf.create(invoice_pdf, options)
-        .then(res => {
-            console.log(res);
-        }).catch(err => {
-            console.log(err);
-        });
+//     const invoiceTemplate = Handlebars.compile(htmlTemplate);
+//     const invoice_pdf = invoiceTemplate(data);
 
-        const filePath = 'http://localhost:3000/downloaded/' + fileName;
+//     pdf.create(invoice_pdf, options).toFile(fileName)
+//         .then(res => {
+//             console.log(res);
+//             console.log(`PDF file saved to ${res.filename}`)
+//         }).catch(err => {
+//             console.log(err);
+//         });
 
-        res.render('download', {
-            path: filePath
-        })
-};
+//         // const filePath = 'http://localhost:3000/downloaded/' + fileName;
+
+//         // res.render('download', {
+//         //     path: filePath
+//         // })
+// };
 
 
 
 // !! Below source code is for generate pdf invoice and save locally on disk
 
-// // Read HTML file and retreive user data from JSON file
-// const htmlTemplate = fs.readFileSync('taxinvoice_template.html', 'utf8');
+// Read HTML file and retreive user data from JSON file
+const htmlTemplate = fs.readFileSync('invoiceTemplate.html', 'utf8');
 
-// const data = JSON.parse(fs.readFileSync('user_data.json', 'utf8'));
-// console.log('user data parsed')
+const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+console.log('user data parsed')
 
-// // Compile HTML file using a template engine (Handlebar)
-// const invoiceTemplate = Handlebars.compile(htmlTemplate);
+// Compile HTML file using a template engine (Handlebar)
+const invoiceTemplate = Handlebars.compile(htmlTemplate);
 
-// const invoice_pdf = invoiceTemplate(data)
-// console.log('complied template with data')
+const invoice_pdf = invoiceTemplate(data)
+console.log('complied template with data')
 
-// // Create invoice as PDF file
-// pdf.create(invoice_pdf).toFile('./taxinvoice1.pdf', 
-// (err, res) => {
-//     if (err) {
-//         return console.log(err);
-//     } else console.log(`PDF file saved to ${res.filename}`);
-//   }
-// );
+// Create invoice as PDF file
+pdf.create(invoice_pdf).toFile('./taxinvoice.pdf', 
+(err, res) => {
+    if (err) {
+        return console.log(err);
+    } else console.log(`PDF file saved to ${res.filename}`);
+  }
+);
